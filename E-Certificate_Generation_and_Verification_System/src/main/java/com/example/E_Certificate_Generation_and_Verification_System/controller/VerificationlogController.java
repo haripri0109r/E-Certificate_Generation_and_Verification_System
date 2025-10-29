085d2ea0766ba58a855d6908d0b7832cd52b7c73
haripri0109r <haripri0109r@gmail.com>
2025-10-29 20:19:37 +0530

    feat:VerificationlogController.java

--- E-Certificate_Generation_and_Verification_System/src/main/java/com/example/E_Certificate_Generation_and_Verification_System/controller/VerificationlogController.java
+++ E-Certificate_Generation_and_Verification_System/src/main/java/com/example/E_Certificate_Generation_and_Verification_System/controller/VerificationlogController.java
@@ -3,14 +3,15 @@ package com.example.E_Certificate_Generation_and_Verification_System.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping; 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.E_Certificate_Generation_and_Verification_System.entity.Verificationlog;
 import com.example.E_Certificate_Generation_and_Verification_System.service.VerificationlogService;
@@ -33,9 +34,17 @@ public class VerificationlogController {
         return verificationlog.map(ResponseEntity::ok)
                               .orElseGet(() -> ResponseEntity.notFound().build());
     }
-    @PostMapping
-    public Verificationlog createVerificationlog(Verificationlog verificationlog) {
+    // JSON API: accept application/json
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Verificationlog createVerificationlog(@RequestBody Verificationlog verificationlog) {
         return verificationlogService.createVerificationlog(verificationlog);
+    }
+
+    // Form submission: redirect to html list
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String createVerificationlogForm(Verificationlog verificationlog) {
+        verificationlogService.createVerificationlog(verificationlog);
+        return "redirect:/verificationlog/list";
     }
 
     
