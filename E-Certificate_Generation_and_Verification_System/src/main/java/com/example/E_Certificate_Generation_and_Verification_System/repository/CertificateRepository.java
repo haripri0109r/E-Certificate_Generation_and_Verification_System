589cc0a8be49a3783684160673e8ae1b07fa4797
haripri0109r <haripri0109r@gmail.com>
2025-10-29 20:21:00 +0530

    feat:CertificateRepository.java

--- E-Certificate_Generation_and_Verification_System/src/main/java/com/example/E_Certificate_Generation_and_Verification_System/repository/CertificateRepository.java
+++ E-Certificate_Generation_and_Verification_System/src/main/java/com/example/E_Certificate_Generation_and_Verification_System/repository/CertificateRepository.java
@@ -1,11 +1,16 @@
 package com.example.E_Certificate_Generation_and_Verification_System.repository;
 
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.E_Certificate_Generation_and_Verification_System.entity.Certificate;
 
-
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
-    
+
+	@Query("SELECT c FROM Certificate c WHERE c.unique_code = :code")
+	Optional<Certificate> findByUniqueCode(@Param("code") String code);
+
 }
