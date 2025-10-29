b115d5bd27d7871b739612359943601cbd1f084b
haripri0109r <haripri0109r@gmail.com>
2025-10-29 09:53:16 +0530

    initial commit:spring-boot initallation for the project

---
+++ E-Certificate_Generation_and_Verification_System/src/main/java/com/example/E_Certificate_Generation_and_Verification_System/ECertificateGenerationAndVerificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_Certificate_Generation_and_Verification_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECertificateGenerationAndVerificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECertificateGenerationAndVerificationSystemApplication.class, args);
+	}
+
+}
