bfb8abda95ed73adecb77e12219b6f367df30af4
haripri0109r <haripri0109r@gmail.com>
2025-10-29 12:35:59 +0530

    feat:VerificationlogController.java

---
+++ E-Certificate_Generation_and_Verification_System/src/main/java/com/example/E_Certificate_Generation_and_Verification_System/controller/VerificationlogController.java
@@ -0,0 +1,43 @@
+package com.example.E_Certificate_Generation_and_Verification_System.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.E_Certificate_Generation_and_Verification_System.entity.Verificationlog;
+import com.example.E_Certificate_Generation_and_Verification_System.service.VerificationlogService;
+
+@RestController
+@RequestMapping("/verificationlogs")
+public class VerificationlogController {
+    
+    @Autowired
+    private VerificationlogService verificationlogService;
+
+    @GetMapping
+    public List<Verificationlog> getAllVerificationlogs() {
+        return verificationlogService.getAllVerificationlogs();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Verificationlog> getVerificationlogById(@PathVariable Long id)    
+    {
+        Optional<Verificationlog> verificationlog = verificationlogService.getVerificationlogById(id);
+        return verificationlog.map(ResponseEntity::ok)
+                              .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public Verificationlog createVerificationlog(Verificationlog verificationlog) {
+        return verificationlogService.createVerificationlog(verificationlog);
+    }
+
+    
+
+}
